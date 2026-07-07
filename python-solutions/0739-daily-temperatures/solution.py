90b3d960bcc6f403517dd93d019045758b6ceb88
RachitS01 <158186859+RachitS01@users.noreply.github.com>
2026-07-07 13:11:29 +0000

    Sync LeetCode submission - Daily Temperatures - Runtime - 89 ms (72.29%), Memory - 28.8 MB (31.48%)

---
+++ python-solutions/0739-daily-temperatures/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer = [0] * len(temperatures)
+        stack = []
+        
+        for i, curr_temp in enumerate(temperatures):
+            
+            while stack and curr_temp > temperatures[stack[-1]]:
+                
+                prev_index = stack.pop()
+                answer[prev_index] = i - prev_index
+                
+            stack.append(i)
+            
+        return answer
