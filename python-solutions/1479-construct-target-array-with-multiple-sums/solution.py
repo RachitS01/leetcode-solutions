0c45d8e639b0a152e167270c94b5a8049cac4d09
RachitS01 <158186859+RachitS01@users.noreply.github.com>
2026-07-09 16:21:48 +0000

    Sync LeetCode submission - Construct Target Array With Multiple Sums - Runtime - 4 ms (87.67%), Memory - 25.2 MB (44.99%)

---
+++ python-solutions/1479-construct-target-array-with-multiple-sums/solution.py
@@ -0,0 +1,31 @@
+import heapq
+class Solution:
+    def isPossible(self, target: List[int]) -> bool:
+
+        if len(target) == 1:
+            return target[0] == 1
+
+        total_sum = sum(target)
+        max_heap = [-x for x in target]
+        heapq.heapify(max_heap)
+
+        while -max_heap[0] > 1:
+            max_val = -heapq.heappop(max_heap)
+
+            rest_sum = total_sum - max_val
+
+            if rest_sum == 1:
+                return True
+            
+            if rest_sum == 0 or max_val <= rest_sum:
+                return False
+
+            prev_val = max_val % rest_sum
+
+            if prev_val == 0:
+                return False
+            
+            heapq.heappush(max_heap,-prev_val)
+            total_sum = prev_val + rest_sum
+        
+        return True
