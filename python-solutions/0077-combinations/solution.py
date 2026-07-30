7a0f915a03703808de5b385f69f8a91a248e9f0e
RachitS01 <158186859+RachitS01@users.noreply.github.com>
2026-07-30 14:25:05 +0000

    Sync LeetCode submission - Combinations - Runtime - 71 ms (95.51%), Memory - 61.2 MB (96.04%)

---
+++ python-solutions/0077-combinations/solution.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        def backtrack(start,path):
+            if len(path) == k:
+                res.append(path[:])
+                return 
+            for num in range(start,n-(k-len(path))+2):
+                path.append(num)
+                backtrack(num+1,path)
+                path.pop()
+        backtrack(1,[])
+        return res
