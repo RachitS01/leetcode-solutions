8de6dc7462d71821e26754e03720bc4d49cb5c6a
RachitS01 <158186859+RachitS01@users.noreply.github.com>
2026-07-30 09:20:29 +0000

    Sync LeetCode submission - Find Kth Bit in Nth Binary String - Runtime - 0 ms (100.00%), Memory - 19.3 MB (74.69%)

---
+++ python-solutions/1667-find-kth-bit-in-nth-binary-string/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findKthBit(self, n: int, k: int) -> str:
+        
+        if n == 1:
+            return "0"
+
+        length = (1<<n) - 1
+        mid = (length//2) + 1
+
+        if k == mid:
+            return "1"
+        if k < mid:
+            return self.findKthBit(n-1,k)
+        else:
+            mirrored_k = length - k + 1
+            left_bit = self.findKthBit(n-1,mirrored_k)
+            return "0" if left_bit == "1" else "1"
+
+    
+    
